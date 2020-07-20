db3ac1dfe92de29d4db92de96d3e29384e122e2f
Marcelo Nogueira Magri <marcelo_magri@hotmail.com>
2020-07-20 18:54:16 -0300

    Initial Commit

---
+++ Chip-8-Emulator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Buffers.Binary;
+using System.IO;
+
+namespace Chip_8_Emulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (BinaryReader reader = new BinaryReader(new FileStream(@"games/GUESS", FileMode.Open)))
+            {
+                while (reader.BaseStream.Position < reader.BaseStream.Length)
+                {
+                    var opcode = (ushort)(reader.ReadByte() << 8 | reader.ReadByte());
+                    Console.WriteLine($"{opcode.ToString("X4")}");
+                }
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
